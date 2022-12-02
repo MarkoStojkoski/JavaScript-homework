40d6d5b3a111f610d6ba5e2d96c3e70addffb9c5
MarkoStojkoski <119707345+MarkoStojkoski@users.noreply.github.com>
2022-12-02 23:05:24 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// Circle area
+let numberPi = 3.14159;
+let numberRadius = 5;
+
+let circleArea = numberPi * (numberRadius * numberRadius);
+ console.log ("Circle area is", circleArea)
+
+//  Price of 30 iphones
+
+let numberIphones = 30;
+let priceOnephone = 119.95 ;
+let taxRate = 5;
+
+let iphonePrice = numberIphones * priceOnephone;
+let taxAmount = (taxRate * iphonePrice) / 100
+let totalPrice = iphonePrice + taxAmount
+
+console.log("The price of 30 iphones is", totalPrice, "$")
+
+
+
+
+
